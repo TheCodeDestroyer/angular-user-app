46aa121e45cf3b2afcb4480936458f28c2d25c68
TheCodeDestroyer <the.code.destroyer@gmail.com>
2014-09-29 20:03:33 +0200

    Done: - Implemented basic print - Implemented randomizing data - Changed a field, partially

--- app/js/index/controllers/indMainCtrl.js
+++ app/js/index/controllers/indMainCtrl.js
@@ -8,23 +8,43 @@ angular.module('aua.controllers').controller('indMainCtrl', ['$scope', 'indUserS
         { sref: 'index.chart', title: 'Chart' }
     ];
 
-    vm.gridData = [];
-
-    indUserSvc.getUsers().then(function (response) {
-        vm.gridData = response.data;
-    });
-
     vm.gridConfig = {
         datatype: 'local',
-        height: 250,
-        colNames:['Id','Full Name', 'Age', 'Sex','Bad luck meter'],
+//        height: 350,
+        height: 'auto',
+        colNames:['Id', 'Full Name', 'Age', 'Sex', 'Bad luck', 'Best quote'],
         colModel:[
             {name:'id', width: 60, sorttype: 'int', hidden: true},
-            {name:'name', width: 250, align: 'center'},
-            {name:'age', width: 100, align: 'center', sorttype: 'int'},
-            {name:'sex', width: 100, align: 'center', sorttype: 'string'},
-            {name:'badLuck', width: 150, align: 'center', sorttype: 'float'}
+            {name:'name', width: 180, align: 'center'},
+            {name:'age', width: 50, align: 'center', sorttype: 'int'},
+            {name:'sex', width: 150, align: 'center', sorttype: 'string'},
+            {name:'badLuck', width: 75, align: 'center', sorttype: 'float'},
+            {name:'bestQuote', width: 180, align: 'center', sorttype: 'string'}
         ]
+    };
+
+    vm.gridData = [];
+    var responseData = [];
+    indUserSvc.getUsers().then(function (response) {
+        responseData = response.data;
+        vm.randomizeData(15);
+    });
+
+    vm.randomizeData = function (numberOfRecords) {
+        var newArrayIds = [];
+        var newArray = [];
+        while(newArrayIds.length < numberOfRecords){
+            var user = responseData[Math.floor(Math.random() * responseData.length)];
+            if (newArrayIds.indexOf(user.id) === -1) {
+                newArrayIds.push(user.id);
+                newArray.push(user);
+            }
+        }
+        vm.gridData = newArray;
+    };
+
+    vm.printData = function () {
+        window.print();
     };
 
 }]);
