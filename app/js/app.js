822886702f38ba01ead55532e4c10aba6ee99c4e
TheCodeDestroyer <the.code.destroyer@gmail.com>
2014-09-29 18:03:04 +0200

    Removed some packages

--- app/js/app.js
+++ app/js/app.js
@@ -8,21 +8,13 @@ angular.module('aua', [
     'ngCookies',
     'ui.router',
     'ui.bootstrap',
-    'pascalprecht.translate',
     'aua.filters',
     'aua.services',
     'aua.directives',
     'aua.controllers'
 ]).
-    config(['$stateProvider', '$urlRouterProvider', '$translateProvider', function($stateProvider, $urlRouterProvider, $translateProvider) {
+    config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
         'use strict';
-
-        $translateProvider.useLocalStorage();
-        $translateProvider.preferredLanguage('en-US');
-        $translateProvider.useStaticFilesLoader({
-            prefix: 'i18n/',
-            suffix: '.json'
-        });
 
         $stateProvider.state('index', {url: '/', abstract: true, templateUrl: 'js/index/partials/indMain.html', controller: 'indMainCtrl as indMain'});
 
